5f6bc35b52882ce118d0c6fddd5245be9ba47dea
huangdaran <787346814@qq.com>
2016-10-26 16:13:20 +0800

    结合了okhttp

---
+++ app/src/main/java/com/huangdaran/rxjavademo/model/TokenModel.java
@@ -0,0 +1,11 @@
+package com.huangdaran.rxjavademo.model;
+
+/**
+ * Created by Administrator on 2016/10/26.
+ */
+
+public class TokenModel {
+    public String status;
+    public String msg;
+    public String token_data;
+}
